9e8a1353d84462bf017bf747c3492bca36a4eef7
Viral <patel.viral@northeastern.edu>
2020-11-22 21:59:31 -0500

    Added sample page warning and link to find match from preferences form

--- src/components/preferences-form/preferences-form.js
+++ src/components/preferences-form/preferences-form.js
@@ -1,14 +1,18 @@
 import React from "react";
 
 import { SectionHeading } from "../styles/common";
-import GenderPreferences from "./questions/gender-preferences/gender-preferences";
+import GenderPreferences from "../../containers/questions/gender-preferences/gender-preferences";
 import { StateProvider } from "../../context/filterContext";
 
-const PreferencesForm = () => {
+const PreferencesForm = (props) => {
   return (
     <StateProvider>
       <div className="container mt-5">
         <div className="row">
+          <SectionHeading className="col-12 alert alert-warning">
+            <h3>This is a sample page</h3>
+          </SectionHeading>
+
           <SectionHeading className="col-12">
             <h1>Set Preferences for Mentor Match</h1>
           </SectionHeading>
@@ -23,7 +27,17 @@ const PreferencesForm = () => {
           <SectionHeading className="col-12 mt-1">
             <h4>3 means you're most comfortable</h4>
           </SectionHeading>
+
           <GenderPreferences />
+
+          <SectionHeading className="col-12 mt-1">
+            <span
+              className="btn btn-link text-decoration-none"
+              onClick={() => props.history.push("/find-match")}
+            >
+              <h2>Find Match</h2>
+            </span>
+          </SectionHeading>
         </div>
       </div>
     </StateProvider>
